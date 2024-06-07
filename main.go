5dd7c9f9398cca90eb1dcf587560cee1c0d65adb
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-06-07 12:54:18 -0300

    ok

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
+// import "github.com/rzjprogramador/RzLibs_GO/soma"
+
+// type ModuleMain struct{
+// 	Soma any
+// }
+
 func main() {}
+
+// func main() ModuleMain {
+// 	module := ModuleMain{
+// 		Soma: soma,
+// 	}
+// 	return module
+// }
