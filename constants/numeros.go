91b7181d54249bc7322607ac4ae3552e7605eef0
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-08-30 11:17:57 -0300

    feat: [constatns] {numbersOptions} subindo

--- constants/numeros.go
+++ constants/numeros.go
@@ -3,15 +3,12 @@ package constants
 type NumbersOptions = int
 
 const (
-	ZERO NumbersOptions = iota
-	UM
-	DOIS
-	TRES
-	QUATRO
-	CINCO
-	SEIS
-	SETE
-	OITO
-	NOVE
-	DEZ
+	ZERO   NumbersOptions = 0
+	UM     NumbersOptions = 1
+	DOIS   NumbersOptions = 2
+	TRES   NumbersOptions = 3
+	QUATRO NumbersOptions = 4
+	CINCO  NumbersOptions = 5
+	SEIS   NumbersOptions = 6
+	SETE   NumbersOptions = 7
 )
