61314b994be32de40b3bc736242b3613ba139149
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-09-05 09:56:27 -0300

    refactor: [MsgTestLIB] usando sprintf golang

--- message/lib.go
+++ message/lib.go
@@ -1,5 +1,7 @@
 package message
 
+import "fmt"
+
 // import (
 // 	"testing"
 // )
@@ -26,26 +28,29 @@ var StaticMessage = Message{
 	Max:        "no máximo",
 }
 
-var molduraMsgTestInit = "\n ***********RESPONSE TEST *************\n"
-var msgEsperado = "Ops... Esperado: %t"
-var msgExpect = "\n Tentativa: %t"
-var molduraMsgTestFinal = "\n*********** FINAL RESPONSE TEST *************\n"
+// var molduraMsgTestInit = "***********RESPONSE TEST *************"
+// var msgCompare = "COMPARE : %s"
+// var msgExpect = "EXPECTED : %s"
+// var molduraMsgTestFinal = "*********** FINAL RESPONSE TEST *************"
 
 // func MessageFN() Message{
 // 	return StaticMessage
 // }
 
-func MessageErrorTestLIB() string {
-	msgFormat := molduraMsgTestInit + msgEsperado + msgExpect + molduraMsgTestFinal
+func MessageErrorTestLIB(compare any, expect any) string {
+	msgFormat := fmt.Sprintf(`
+
+		***********RESPONSE TEST *************
+
+		COMPARE : %s
+
+		EXPECTED : %s
+
+		*********** FINAL RESPONSE TEST *************
+
+		`, compare, expect)
 
 	res := msgFormat
 
 	return res
-
-	// return "\n ***********RESPONSE TEST *************\n Ops... Esperado: %t --> Tentativa: %t \n*********** FINAL RESPONSE TEST ************* "
 }
-
-// func MessageErrorTest(compare any, expect any)  {
-// 	t.Error("\n ***********RESPONSE TEST *************\n Ops... Esperado: %t --> Tentativa: %t \n*********** FINAL RESPONSE TEST ************* ", expect, compare)
-
-// }
