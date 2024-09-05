3b98df6ce32157cb74fd4c54c404d1120e6f5551
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-09-05 09:05:46 -0300

    refactor: add pacote na raiz para ver se consigo reusa-lo remotamente

--- message/lib.go
+++ message/lib.go
@@ -1,5 +1,11 @@
 package message
 
+// import (
+// 	"testing"
+// )
+
+// var t *testing.T
+
 type Message struct {
 	Sucess     string
 	Fail       string
@@ -20,4 +26,13 @@ var StaticMessage = Message{
 
 // func MessageFN() Message{
 // 	return StaticMessage
+// }
+
+func LibMessageErrorTest() string {
+	return "\n ***********RESPONSE TEST *************\n Ops... Esperado: %t --> Tentativa: %t \n*********** FINAL RESPONSE TEST ************* "
+}
+
+// func MessageErrorTest(compare any, expect any)  {
+// 	t.Error("\n ***********RESPONSE TEST *************\n Ops... Esperado: %t --> Tentativa: %t \n*********** FINAL RESPONSE TEST ************* ", expect, compare)
+
 // }
