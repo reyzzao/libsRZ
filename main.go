78876d3f136084839e6c2a990f3b50babcdf2f01
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2024-09-05 09:06:43 -0300

    refactor: add pacote [message] na raiz para ver se consigo reusa-lo remotamente

--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"github.com/reizzao/RzLibs_GO/feedback"
 	"github.com/reizzao/RzLibs_GO/message"
-	"github.com/reizzao/RzLibs_GO/message/message_bytest"
 )
 
 func main() {
 
 	feedback.TesterFeedback()
 	message.TesterMessage()
-	message_bytest.LibMessageErrorTest()
+	message.LibMessageErrorTest()
 }
 
 // importante: tem que chamar a lib aqui na main - para poderem acessa-la remotamente
