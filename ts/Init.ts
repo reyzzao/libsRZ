86bf87b61e07d885b55a9c2bfa7ff97ad1c059c4
reizzao <reizao1977@gmail.com>
2025-08-04 15:05:04 -0300

    ok

--- ts/Init.ts
+++ ts/Init.ts
@@ -1,17 +1,16 @@
 // import { libConsole } from './console/Lib.ts'
 
 
-// export interface ILibTS {
-//   console: {
-//     libConsole: (...data: any[]) => void
-//   }
-// }
+export interface ILibTS {
+  test: string,
+  fn: (d: string) => string
+}
 
 // export const libConsole = (...data: any[]): void => console.log(...data) 
-const testFN = () => "retorno testFN --1"
+const testFN = (d: string) => d
 
 // Porta de entrada
-export const LibTS = {
+export const LibTS: ILibTS = {
   test: "foo1000",
   fn: testFN
 }
