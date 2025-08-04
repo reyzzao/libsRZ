215319fc985049d135b794cfab607b0df63c2701
reizzao <reizao1977@gmail.com>
2025-08-04 15:12:43 -0300

    feat: tentativa console

--- ts/Init.ts
+++ ts/Init.ts
@@ -3,14 +3,16 @@
 
 export interface LibTS {
   test: string,
-  fn: (d: string) => string
+  fn: (d: string) => string,
+  console: (...data: any[]) => void
 }
 
-// export const libConsole = (...data: any[]): void => console.log(...data) 
+const libConsole = (...data: any[]): void => console.log(...data) 
 const testFN = (d: string) => d
 
 // Porta de entrada
 export const LibTS: LibTS = {
   test: "foo1000",
-  fn: testFN
+  fn: testFN,
+  console: (...data: any[]): void => console.log(...data) 
 }
